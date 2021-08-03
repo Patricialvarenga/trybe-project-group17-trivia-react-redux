429325b923de63e62341b42f2799893e29c97ed9
Merlone <alestori7@gmail.com>
2021-08-03 17:12:04 -0300

    Corrige lint

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Login from './pages/Login';
 export default function App() {
   return (
     <Switch>
-      <Route exact path="/" component={Login}/>
+      <Route exact path="/" component={ Login } />
     </Switch>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
 import store from './store';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={ store }>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
- );
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
